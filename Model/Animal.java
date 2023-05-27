56320dac3371678128b4addd014678eb1dd4a335
Rinkiller <Rinkiller@mail.ru>
2023-05-27 21:09:03 +0300

    first commit

---
+++ Model/Animal.java
@@ -0,0 +1,12 @@
+package Model;
+
+import java.util.List;
+
+public interface Animal {
+    public String getName();
+
+    public List<String> getCommands();
+
+    public void setCommands(String string);
+
+}
---
+++ Model/Main.java
@@ -0,0 +1,14 @@
+package Model;
+
+import Presenter.Presenter;
+import View.View;
+
+public class Main {
+    public static void main(String[] args) {
+        Model model = new Model();
+        View view = new View();
+        Presenter presenter = new Presenter(model, view);
+        presenter.run();
+
+    }
+}
